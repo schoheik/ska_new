95bf98eb66533f805c400dbbedd7e5ccc84009d8
HI K O <schoheik@googlemail.com>
2026-06-30 13:19:00 +0200

    tour: load flyer images from CSV on row click, show on right panel; switch to JS data file for file:// compat; add new images and cleanup

---
+++ res/tour.data.js
@@ -0,0 +1,4 @@
+var tourData = `Date;Location;Info;Link;Flyer
+04.07.2026;Zeil am Main; Göller;https://www.brauerei-goeller.de/; Plakat_Göller.jpeg
+12.02.1991;Bamberg;Heiko's Birthday;www.google.de;Heiko.jpg
+24.12.0001;Jerusalem;Jesus Birthday;www.gott.de;Weihnachten.jpg`;
