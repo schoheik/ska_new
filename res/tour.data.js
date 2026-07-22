a05e301adbde3ff1b51880805e6ade1cc6c5e69c
HI K O <schoheik@googlemail.com>
2026-07-22 14:22:30 +0200

    Update tour data

--- res/tour.data.js
+++ res/tour.data.js
@@ -1,5 +1,5 @@
 var tourData = `Date;Location;Info;Link;Flyer
 04.07.2026;Zeil am Main; Göller;https://www.brauerei-goeller.de/; Plakat_Göller.jpeg
-12.02.1991;Bamberg;Heiko's Birthday;www.google.de;Heiko.jpg
+12.02.1991;Bamberg;Heiko's Birthday;;Heiko.jpg
 24.12.0001;Jerusalem;Jesus Birthday;www.gott.de;Weihnachten.jpg
 30.08.2026;Höchstadt an der Aisch;Altstadtfest;www.hoechstadt.de;;`
